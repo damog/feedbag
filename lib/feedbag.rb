fae3019476757691429d6e502bc516b2483dff40
David Moreno <david@axiombox.com>
2009-04-22 11:14:11 -0400

    0.5.13

--- lib/feedbag.rb
+++ lib/feedbag.rb
@@ -50,7 +50,7 @@ module Feedbag
 		end
 	end
 
-	def self.find(url)
+	def self.find(url, args = nil)
 		$feeds = []
 
 		url_uri = URI.parse(url)
@@ -65,20 +65,22 @@ module Feedbag
 		#url = "#{url_uri.scheme or 'http'}://#{url_uri.host}#{url_uri.path}"
 
 		# check if feed_valid is avail
-		begin
-			require "feed_validator"
-			v = W3C::FeedValidator.new
-			v.validate_url(url)
-			return self.add_feed(url, nil) if v.valid?
-		rescue LoadError
-			# scoo
-		rescue REXML::ParseException
-		  # usually indicates timeout
-		  # TODO: actually find out timeout. use Terminator?
-		  $stderr.puts "Feed looked like feed but might not have passed validation or timed out"
-    rescue => ex
-			$stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
-		end
+    unless args[:narrow]
+      begin
+	  		require "feed_validator"
+		  	v = W3C::FeedValidator.new
+			  v.validate_url(url)
+  			return self.add_feed(url, nil) if v.valid?
+	  	rescue LoadError
+		  	# scoo
+  		rescue REXML::ParseException
+	  	  # usually indicates timeout
+		    # TODO: actually find out timeout. use Terminator?
+		    $stderr.puts "Feed looked like feed but might not have passed validation or timed out"
+      rescue => ex
+	  		$stderr.puts "#{ex.class} error ocurred with: `#{url}': #{ex.message}"
+		  end
+    end
 
 		begin
 			html = open(url) do |f|
@@ -102,19 +104,21 @@ module Feedbag
 					end
 				end
 
-				(doc/"a").each do |a|
-					next unless a["href"]
-					if self.looks_like_feed?(a["href"]) and (a["href"] =~ /\// or a["href"] =~ /#{url_uri.host}/)
-						self.add_feed(a["href"], url, $base_uri)
-					end
-				end
+        unless args[:narrow]
+  				(doc/"a").each do |a|
+	  				next unless a["href"]
+		  			if self.looks_like_feed?(a["href"]) and (a["href"] =~ /\// or a["href"] =~ /#{url_uri.host}/)
+			  			self.add_feed(a["href"], url, $base_uri)
+				  	end
+  				end
 
-				(doc/"a").each do |a|
-					next unless a["href"]
-					if self.looks_like_feed?(a["href"])
-						self.add_feed(a["href"], url, $base_uri)
-					end
-				end
+	  			(doc/"a").each do |a|
+		  			next unless a["href"]
+			  		if self.looks_like_feed?(a["href"])
+				  		self.add_feed(a["href"], url, $base_uri)
+					  end
+  				end
+        end
 
 			end
 		rescue Timeout::Error => err
